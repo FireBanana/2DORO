632f2b945bdfbfe5268ce64992c894b93ec09030
DESKTOP-AMT79IF\Owais <free0run@gmail.com>
2018-03-04 17:11:12 +0500

    Initial Commit

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : Player
+{
+
+    RaycastHit2D hit;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+
+        if (playerAction == playerState.rolling)
+        {
+            rollMovement();
+            return;
+        }
+
+        if (playerAction == playerState.jumping)
+        {
+            if (Physics2D.Raycast(transform.position, -transform.up, 0.19f, 1 << 8))
+            {
+                playerAction = playerState.idle;
+                Debug.Log("asd");
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (playerAction != playerState.jumping)
+            {
+                playerAction = playerState.jumping;
+                jump(4);
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (playerAction == playerState.jumping)
+            {
+                moveLeft();
+            }
+            else
+            {
+                if (isRunning == true)
+                {
+                    playerAction = playerState.running;
+                    speed = 0.03f;
+                    moveLeft();
+                    anim.SetBool("RunTrigger", true);
+                }
+                else
+                {
+                    playerAction = playerState.walking;
+                    speed = 0.01f;
+                    moveLeft();
+                    anim.SetBool("WalkTrigger", true);
+                }
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (playerAction == playerState.jumping)
+            {
+                moveRight();
+            }
+            else
+            {
+                if (isRunning == true)
+                {
+                    playerAction = playerState.running;
+                    speed = 0.03f;
+                    moveRight();
+                    anim.SetBool("RunTrigger", true);
+                }
+                else
+                {
+                    playerAction = playerState.walking;
+                    speed = 0.01f;
+                    moveRight();
+                    anim.SetBool("WalkTrigger", true);
+                }
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetBool("WalkTrigger", false);
+            anim.SetBool("RunTrigger", false);
+            playerAction = playerState.idle;
+            isRunning = false;
+        }
+        if (playerAction == playerState.jumping)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (leftDash == true)
+            {
+                isRunning = true;
+            }
+            leftDashCheck();
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (rightDash == true)
+            {
+                isRunning = true;
+            }
+            rightDashCheck();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed = 0.05f;
+            anim.SetBool("WalkTrigger", false);
+            anim.SetBool("RunTrigger", false);
+            playerAction = playerState.rolling;
+            anim.SetBool("RollTrigger", true);
+            roll("Fighter_roll", 0.01f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    string Class;
+    GameObject weapon;
+    GameObject eChip;
+    GameObject discharger;
+    Rigidbody2D rb;
+    protected Animator anim;
+    SpriteRenderer sr;
+
+    protected enum playerState { idle, running, rolling, jumping, walking}
+    protected playerState playerAction;
+
+    [SerializeField]
+    float power;
+    [SerializeField]
+    float intelligence;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    float jumpHeight;
+    [SerializeField]
+    float defense;
+    [SerializeField]
+    float defaultSpeed;
+    float dashDuration = 0.2f;
+    protected bool leftDash, rightDash, isRunning;
+
+    public void Initialize()
+    {
+        playerAction = playerState.idle;
+        rb = GetComponent<Rigidbody2D>();
+        speed = 0.01f;
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    } 
+
+    public void jump(float jumpPow)
+    {
+        rb.AddForce(new Vector2(0, jumpPow), ForceMode2D.Impulse);
+    }
+
+    public void moveLeft()
+    {
+        transform.Translate(new Vector2(-1, 0) * speed);
+        sr.flipX = true;
+    }
+    public void moveRight()
+    {
+        transform.Translate(new Vector2(1, 0) * speed);
+        sr.flipX = false;
+    }
+
+    public void rollMovement()
+    {
+        if (sr.flipX == true)
+        {
+            transform.Translate(new Vector2(-1, 0) * speed);
+        }
+        else
+        {
+            transform.Translate(new Vector2(1, 0) * speed);
+        }
+    }
+
+    public void roll(string rollAnimName, float normSpeed)
+    {
+        defaultSpeed = normSpeed;
+        anim.Play(rollAnimName);
+        StartCoroutine("waitForRoll");
+
+    }
+    public void leftDashCheck()
+    {
+        if (leftDash == false)
+        {
+            StartCoroutine(dashDelay(1));
+            leftDash = true;
+        }
+    }
+    public void rightDashCheck()
+    {
+        if (rightDash == false)
+        {
+            StartCoroutine(dashDelay(2));
+            rightDash = true;
+        }
+    }
+
+    IEnumerator waitForRoll()
+    {
+        yield return new WaitForSeconds(0.5f);
+        speed = defaultSpeed;
+        anim.SetBool("RollTrigger", false);
+        playerAction = playerState.idle;
+    }
+
+    IEnumerator dashDelay(int i)
+    {
+        yield return new WaitForSeconds(dashDuration);
+        if(i == 1)
+        {
+            leftDash = false;
+        }
+        if(i == 2)
+        {
+            rightDash = false;
+        }
+    }
+
+}
